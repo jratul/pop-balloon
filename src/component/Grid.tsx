4478e1c0f19f1244b3ddd7cf5a054491d027c10d
jratul <jratul@nate.com>
2024-12-12 20:14:41 +0900

    chg input range to react range

--- src/component/Grid.tsx
+++ src/component/Grid.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
+import { Range } from "react-range";
 import styled from "@emotion/styled";
 import { Pos } from "@model/model";
 import useGrid from "@hook/useGrid";
@@ -8,6 +9,7 @@ import Button from "./Button";
 import Highlight from "./Highlight";
 import Loading from "./Loading";
 import { ClearAlert, FailAlert } from "./GridAlert";
+import { defaultSizeValue, maxSizeValue } from "src/constant";
 
 const Title = styled.h1`
   text-align: center;
@@ -41,7 +43,7 @@ export default function Grid() {
   const { size, balloons, maxCount, popBalloon, resetBalloons, failed } =
     useGrid();
 
-  const sizeInputRef = useRef<HTMLInputElement>(null);
+  const [inputSize, setInputSize] = useState<number>(defaultSizeValue);
 
   const arr = Array.from(Array(size.height), () => Array(size.width).fill(0));
 
@@ -50,9 +52,13 @@ export default function Grid() {
   };
 
   const handleReset = () => {
-    if (sizeInputRef?.current) {
-      const value = parseInt(sizeInputRef?.current?.value) as number;
-      resetBalloons({ height: value, width: value });
+    resetBalloons({ height: inputSize, width: inputSize });
+  };
+
+  const handleInputSizeChange = (newValues: number[]) => {
+    const newValue = newValues[0];
+    if (newValue >= 1 && newValue <= maxSizeValue) {
+      setInputSize(newValue);
     }
   };
 
@@ -74,18 +80,43 @@ export default function Grid() {
         <Highlight>P</Highlight>op <Highlight>B</Highlight>alloon
       </Title>
       <Topper>
-        <input
-          type="number"
+        <Range
           min={1}
           max={10}
           step={1}
-          defaultValue={size.width}
-          ref={sizeInputRef}
+          values={[inputSize]}
+          onChange={handleInputSizeChange}
           disabled={loading}
+          renderTrack={({ props, children }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: "6px",
+                width: "200px",
+                backgroundColor: "#fca5a5",
+              }}
+            >
+              {children}
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              key={props.key}
+              style={{
+                ...props.style,
+                height: "20px",
+                width: "20px",
+                borderRadius: "100%",
+                backgroundColor: "#ef4444",
+              }}
+            />
+          )}
         />
         <Button disabled={loading} onClick={handleReset}>
           <img src="/image/reset.svg" width={20} height={20} />
-          다시하기
+          다시하기({inputSize} x {inputSize})
         </Button>
       </Topper>
       <Container>
